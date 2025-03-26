4ea948717ffbac0ba198229c1c2840f2f32b18bd
Gabriel Miranda <gsigm4@gmail.com>
2025-03-26 16:10:36 -0400

    Operadores Logicos

--- tipos-variaveis/src/OperadoresIntro.java
+++ tipos-variaveis/src/OperadoresIntro.java
@@ -2,17 +2,17 @@ public class OperadoresIntro {
     public static void main(String[] args) {
        //Estudo sobre alguns tipos de operadores e suas funcoes dentro de um programa java
        // Nota **Pensar em operadores como "acoes" que podemos fazer com variaveis (somar comparar etc..)
-      //Operadores de comparacao (retornar true or false)
-      int idade = 18;
-      boolean temCarteira = true;
+      //Operadores Logicos irao combinar condicoes
 
-      // Pode dirigir? (idade >= 18 E temCarteira)
-      boolean podeDirigir = (idade >= 18) && temCarteira;
-      System.out.println("Pode dirigir? " + podeDirigir); // true
+      String user = "admin";
+      String password = "1234";
+      boolean correctUser = user.equals("admin"); // Compara Strings
+      boolean correctPassword = password.equals("1234"); 
+      boolean wrongPassword = password.equals("5678");
 
-      // É menor de idade ou não tem carteira?
-      boolean precisaDeAutorizacao = (idade < 18) || !temCarteira;
-      System.out.println("Precisa de autorização? " + precisaDeAutorizacao); 
-        
+      // Pode logar (user And password correct?)
+      boolean canLogIn = correctUser && correctPassword ;
+      System.out.println("Guaranteed Access? " + canLogIn); //false
+
     }
 }
