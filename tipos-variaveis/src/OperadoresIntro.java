be2d066b6f293b1e1001fde46643b92155d6fc5c
Gabriel Miranda <gsigm4@gmail.com>
2025-03-26 15:55:42 -0400

    operador comparação

--- tipos-variaveis/src/OperadoresIntro.java
+++ tipos-variaveis/src/OperadoresIntro.java
@@ -2,20 +2,17 @@ public class OperadoresIntro {
     public static void main(String[] args) {
        //Estudo sobre alguns tipos de operadores e suas funcoes dentro de um programa java
        // Nota **Pensar em operadores como "acoes" que podemos fazer com variaveis (somar comparar etc..)
-      //Proximo exercico operador de atribuicao | contador
-      int saldo = 100;
+      //Operadores de comparacao (retornar true or false)
+      int idade = 18;
+      boolean temCarteira = true;
 
-      saldo += 50; //depositar 50
-      System.out.println("Saldo após deposito: " + saldo); //150
+      // Pode dirigir? (idade >= 18 E temCarteira)
+      boolean podeDirigir = (idade >= 18) && temCarteira;
+      System.out.println("Pode dirigir? " + podeDirigir); // true
 
-      saldo -= 30; // sacar 30
-      System.out.println("Saldo após saque: " + saldo); // 120
-
-      saldo *= 2; // dobrar o saldo
-      System.out.println("Saldo após dobrado: " + saldo); // 240
-    
-      saldo *= 1.10; // 10% de juros
-      System.out.println("Saldo após 10% de juros: " + saldo); //264 (240 + 10%)
+      // É menor de idade ou não tem carteira?
+      boolean precisaDeAutorizacao = (idade < 18) || !temCarteira;
+      System.out.println("Precisa de autorização? " + precisaDeAutorizacao); 
         
     }
 }
