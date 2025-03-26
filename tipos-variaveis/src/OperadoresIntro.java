45cb9a3831d18133d70f229c5b0b6734312cd4e6
Gabriel Miranda <gsigm4@gmail.com>
2025-03-26 15:45:35 -0400

    Operador de atribuição

--- tipos-variaveis/src/OperadoresIntro.java
+++ tipos-variaveis/src/OperadoresIntro.java
@@ -2,16 +2,20 @@ public class OperadoresIntro {
     public static void main(String[] args) {
        //Estudo sobre alguns tipos de operadores e suas funcoes dentro de um programa java
        // Nota **Pensar em operadores como "acoes" que podemos fazer com variaveis (somar comparar etc..)
-       // comecarei com uma calculadora simples
-       int a = 15;
-       int b = 4; 
+      //Proximo exercico operador de atribuicao | contador
+      int saldo = 100;
 
-       //calcular e imprimir
-       System.out.println("Soma: " + (a + b));      //resultado esperado 19
-       System.out.println("Subtração: " + (a - b)); //resultado esperado 11
-       System.out.println("Multiplicação: " + (a * b)); //resultado esperado 60
-       System.out.println("Divisão: " + ((double)a / b)); // necessario utilizar double para numeros "quebrados"
-       System.out.println("Resto: " + (a % b)); //resultado esperado 3
+      saldo += 50; //depositar 50
+      System.out.println("Saldo após deposito: " + saldo); //150
 
+      saldo -= 30; // sacar 30
+      System.out.println("Saldo após saque: " + saldo); // 120
+
+      saldo *= 2; // dobrar o saldo
+      System.out.println("Saldo após dobrado: " + saldo); // 240
+    
+      saldo *= 1.10; // 10% de juros
+      System.out.println("Saldo após 10% de juros: " + saldo); //264 (240 + 10%)
+        
     }
 }
